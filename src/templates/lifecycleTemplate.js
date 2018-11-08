fae2a7cd9e23aef5d8a9760fa4df12b742140bb7
JimmyBeldone <dev.jimmy.beldone@gmail.com>
2018-11-08 14:27:00 +0100

    Updated Deps

--- src/templates/lifecycleTemplate.js
+++ src/templates/lifecycleTemplate.js
@@ -12,19 +12,10 @@ export default () => (`
 
     }
 
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        return null
-    }
-
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (snapshot !== null) {
 
         }
-    }
-
-    componentDidCatch(error, info) {
-        console.log(error);
-        this.setState(state => ({...state, hasError: true}))
     }
 
     componentWillUnmount() {
